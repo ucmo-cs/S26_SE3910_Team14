e84a826a547b58044cb078a9cfb6fe8badcb5499
ItsMeChef <JenkinsJ2703@gmail.com>
2026-04-22 03:11:25 -0500

    db seeding error fix
    db seeding error fix

--- backend/src/main/java/com/bankscheduling/appointment/repository/EmployeeRepository.java
+++ backend/src/main/java/com/bankscheduling/appointment/repository/EmployeeRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByUsername(String username);
+    Optional<Employee> findByWorkEmail(String workEmail);
 
     @Query("select e from Employee e join fetch e.role where e.username = :username")
     Optional<Employee> findByUsernameForAuth(@Param("username") String username);
